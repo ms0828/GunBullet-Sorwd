6c818e6a9148c484cac5a7b757ad58cf0e5afd7b
hsjsstn <105033044+hsjsstn@users.noreply.github.com>
2022-07-27 18:34:16 +0900

    7.27 팔이 총으로 된 적 (미완성)
    .

--- Assets/MyScripts/ArmMachine.cs
+++ Assets/MyScripts/ArmMachine.cs
@@ -10,12 +10,13 @@ public class ArmMachine : Enemy
 
     int playerLayer;
     float direct;
-    public float knockbackSpeed = 3;     // 넉백 속도 조절
-    public float knockbackTime = 0.2f;       // 넉백 시간 조절
-    public float snipingTime = 3f;      // 저격 시간 조절
+    public float knockbackTime = 1.0f;       // 넉백 시간 조절
+    public float snipingTime = 4.0f;      // 저격 시간 조절
 
     public bool isknockback = false;
     public bool isSniping = false;
+    public bool isKnifeCoolTime = false;
+    public bool isSnipingCoolTime = false;    
 
     new void Start()
     {
@@ -25,70 +26,88 @@ public class ArmMachine : Enemy
         currentHp = 100;
         speed = 1f;
         shortAttackPower = 15;
-        longAttackPower = 20;
+        longAttackPower = 10;
 
         playerLayer = 1 << LayerMask.NameToLayer("Player");
-
-
     }
 
     new void FixedUpdate()
     {
         base.FixedUpdate();
 
-        ShortAttack();
+        // ShortAttack();
+        LongAttack(); 
     }
 
 
 
     // ----- 적 근거리 공격 후 넉백 관련 -----
-    void Knockback(float dir)
+    void Knockback()
     {
         isknockback = true;
         float time = 0;
+
         
+        Debug.Log(transform.position.x - player.position.x);
+
         while (time < knockbackTime)
         {
-            if (transform.rotation.y == 0)      // 플레이어 위치의 반대 방향으로 넉백      
-                transform.Translate(Vector2.left * knockbackSpeed * Time.deltaTime * dir);
+            if(transform.position.x - player.position.x > 0)    // 플레이어 위치의 반대 방향으로 넉백                    
+                rb.AddForce(new Vector2(0.4f,0f), ForceMode2D.Impulse);
             else
-                transform.Translate(Vector2.left * knockbackSpeed * Time.deltaTime * -1f * dir);
+                rb.AddForce(new Vector2(-0.4f,0f), ForceMode2D.Impulse);  
         
             time += Time.deltaTime;
         }
 
         isknockback = false;
     }
-
+    // ----- 적 원거리 공격 관련 -----
     void Sniping()
     {
         isSniping = true;
+        speed = 0;      // 총을 조준하는 동안 적은 움직이지 않음
         float time = 0;
 
-        while (time < snipingTime)      // 레이저 조준하는 시간
-        {
+        am.SetTrigger("Sniping");
+
+        while (time < snipingTime)      // 총을 조준하는 시간 (레이저가 발사됨)
+        {   
             time += Time.deltaTime;
         }
 
+        speed = 1f;
         isSniping = false;
+    }
+    
+    // ----- 쿨타임 관련 -----
+        IEnumerator KnifeCoolTime()
+    {
+        isKnifeCoolTime = true;
+
+        yield return new WaitForSeconds(5.0f);
+
+        isKnifeCoolTime = false;
+    }
+        IEnumerator SnipingCoolTime()
+    {
+        isSnipingCoolTime = true;
+
+        yield return new WaitForSeconds(8.0f);
+
+        isSnipingCoolTime = false;
     }
 
     public override void ShortAttack()     // 근접 공격
     {
         Collider2D collider1 = Physics2D.OverlapBox(hitBox.position,hitBox.localScale,0,playerLayer);
         
-        if(collider1 != null)    // 플레이어가 있으면 
+        if(collider1 != null && isKnifeCoolTime == false)    // 히트박스 범위 내에 플레이어가 있으면 
         {   
+            StartCoroutine(KnifeCoolTime());
             am.SetTrigger("KnifeAttack");
-
-            if(transform.position.x - player.position.x > 0)  // 대상이 오른쪽에 있다면
-            {
-                Knockback(1);       
-            }
-            else      // 대상이 왼쪽에 있다면
-            {
-                Knockback(-1);  
-            }
+            collider1.GetComponent<ITakeDamage>().TakeDamage(player.transform, shortAttackPower);
+            Knockback();            
         }
     }
 
@@ -96,14 +115,16 @@ public class ArmMachine : Enemy
     {
         Collider2D collider2 = Physics2D.OverlapBox(SnipingScope.position,SnipingScope.localScale,0,playerLayer);
 
-        if(collider2 != null)
+        if(collider2 != null && isSnipingCoolTime == false)
         {
+            StartCoroutine(SnipingCoolTime());
             am.SetTrigger("Sniping");
-
+            // collider2.GetComponent<ITakeDamage>().TakeDamage(player.transform, longAttackPower);
             Sniping();
         }
     }
 
+        // ----- 원거리 저격 범위 -----
         private void OnDrawGizmos()     
     {                               
         Gizmos.color = Color.yellow;
