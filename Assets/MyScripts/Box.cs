6a9baf6af9abf2b936bb7ba016df24200db35712
SinMuSeoung <musetng1@naver.com>
2022-08-23 02:24:03 +0900

    회복 박스
    .

--- Assets/MyScripts/Box.cs
+++ Assets/MyScripts/Box.cs
@@ -2,42 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Box : MonoBehaviour
+public class Box : MonoBehaviour, IInterfaceObject
 {    
-    public GameObject box;
-    public GameObject gKey;
     public Player player;
 
-    public bool isTriggerEnter = false;
 
-    void Update() 
+    void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.G) && isTriggerEnter == true) // G키를 누르면
-        {
-            Destroy(box); // 박스가 사라짐
-        }
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) // 플레이어가 가까이 오면
-    {
-        if(other.gameObject.name.Equals("Player")) 
-        {
-            if (this.gameObject.tag.Equals("Stage1"))
-            {
-                gKey.SetActive(true); // G키 이미지를 띄움
-            }
 
-            isTriggerEnter = true;
+    public void Interface()
+    {
+        player.currentHp += 30;
+        if(player.currentHp > player.maxHp)
+        {
+            player.currentHp = player.maxHp;
         }
+
+        player.playerUi.SetPlayerHpBar(player.currentHp);
+
+        Destroy(gameObject);
     }
 
-    private void OnTriggerExit2D(Collider2D other) // 플레이어가 멀어지면
-    {
-        if(other.gameObject.name.Equals("Player")) 
-        {
-            gKey.SetActive(false); // G키 이미지 삭제      
-
-            isTriggerEnter = false;
-        }
-    }
+    
 }
