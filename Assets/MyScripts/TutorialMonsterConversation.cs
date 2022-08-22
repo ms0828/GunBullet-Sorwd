f11d758ecdbba1b56844459177957d910cf95238
SinMuSeoung <musetng1@naver.com>
2022-08-23 03:11:01 +0900

    수정본
    폰트 적용, UI, 타이틀 위치

--- Assets/MyScripts/TutorialMonsterConversation.cs
+++ Assets/MyScripts/TutorialMonsterConversation.cs
@@ -6,12 +6,12 @@ public class TutorialMonsterConversation : ConversationObject
 {
     void Awake()
     {
-        speaker = "Monster";
+        speaker = "???";
         content = new string[6];
         content[0] = "크크큭...";
         content[1] = "네 동료들과 가족들이 이렇게 된 것이 억울한가?";
         content[2] = "그렇다면 물어보지";
-        content[3] = "누가 이런 상황을 만들었다고 생각하는가?";
+        content[3] = "누가 이런 상황을 만들었다고 생각하는가!";
         content[4] = "이 전쟁이 꼭 일어났어야만 했는가?";
         content[5] = "다- 네 탓이란 말이다!! 크하하하!!";
     } 
